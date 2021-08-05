5d55b30356e5356a09a1428719bd933deea7d8f5
Latrell <lll4xy@virginia.edu>
2021-08-04 21:08:42 -0400

    added in basic methods outlined in lecture, will do again tomorrow

--- lists2/src/SLList.java
+++ lists2/src/SLList.java
@@ -2,14 +2,29 @@
  * of the nakedness within. */
 
 public class SLList {
+
+    private class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+
+    }
     private IntNode first;
+    private int size;
+
     public SLList(int x) {
         first = new IntNode(x, null);
+        size = 1;
     }
 
     public void addFirst(int x) {
         // goal is to add a node at the beginning of the list
         first = new IntNode(x, first);
+        size += 1;
     }
 
     public int getFirst() {
@@ -17,11 +32,40 @@ public class SLList {
         return first.item;
     }
 
+    public void addLast(int x) {
+        // loop through the list (recursively or iteratively) until first.next is null
+
+        IntNode curr = first;
+
+        // move the pointer to the end, good job Latrell!
+        while(curr.next != null) {
+            curr = curr.next;
+        }
+
+        curr.next = new IntNode(x, null);// make sure to always add a node
+        size += 1;
+    }
+
+//    /** Returns the size of the list that starts at IntNode p */
+//    private static int size(IntNode p) {
+//        if (p.next == null) {
+//            return 1;
+//        }
+//
+//        return 1 + size(p.next);
+//    }
+
+    public int size() {
+        return size;
+        //return 1 + first.size(); // no S list pointers so need a helper method
+    }
+
     public static void main(String[] args) {
         /* Creates a list of one integer, namely 10 */
         SLList l = new SLList(15);
         l.addFirst(10);
         l.addFirst(5);
-        System.out.println(l.getFirst());
+        //System.out.println(l.getFirst());
+        l.addLast(7);
     }
 }
